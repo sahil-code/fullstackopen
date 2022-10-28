e33d4c7f52817083c7e451fa32db9b965c3fb956
Sahil A <sahilagarwal4b@gmail.comA>
2022-10-28 23:31:32 +0530

    ongoing 8b

---
+++ part8/library-frontend/src/App.js
@@ -0,0 +1,25 @@
+import { gql, useQuery } from '@apollo/client'
+import Persons from './Persons'
+
+const ALL_PERSONS = gql`
+  query {
+    allPersons {
+      name
+      phone
+      id
+    }
+  }
+`
+
+const App = () => {
+  const result = useQuery(ALL_PERSONS)
+
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+
+  return <Persons persons={result.data.allPersons} />
+}
+
+export default App
+
