63818e001b816a458eb03adc61570c3318f81e88
Sahil A <sahilagarwal4b@gmail.comA>
2022-11-01 23:26:09 +0530

    ready for 8.8

--- part8/library-frontend/src/App.js
+++ part8/library-frontend/src/App.js
@@ -1,24 +1,40 @@
-import { gql, useQuery } from '@apollo/client'
-import Persons from './Persons'
-
-const ALL_PERSONS = gql`
-  query {
-    allPersons {
-      name
-      phone
-      id
-    }
-  }
-`
+import { useQuery } from '@apollo/client'
+import Persons from './components/Persons'
+import PersonForm from './components/PersonForm'
+import { ALL_PERSONS } from './queries'
+import { useState } from 'react'
+import PhoneForm from './components/PhoneForm'
 
 const App = () => {
+  const [errorMessage, setErrorMessage] = useState(null)
   const result = useQuery(ALL_PERSONS)
 
   if (result.loading) {
     return <div>loading...</div>
   }
 
-  return <Persons persons={result.data.allPersons} />
+  const notify = (message) => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000)
+  }
+
+  return (
+    <div>
+      <Notify errorMessage={errorMessage} />
+      <Persons persons={result.data.allPersons} />
+      <PersonForm setError={notify} />
+      <PhoneForm setError={notify} />
+    </div>
+  )
+}
+
+const Notify = ({ errorMessage }) => {
+  if (!errorMessage) {
+    return null
+  }
+  return <div style={{ color: 'red' }}> {errorMessage} </div>
 }
 
 export default App
